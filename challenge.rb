21f821a74bb1acc64882d840429462c9ba63433a
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-04-26 20:36:57 +0000

    solved challenge

--- challenge.rb
+++ challenge.rb
@@ -8,3 +8,16 @@ fruits = ["apples", "oranges", "bananas", "pears"]
 for_each_in(fruits) do |f|
   puts "I like #{f}."
 end
+
+
+function for_each_in(array, instructions) {
+        instructions();
+    }
+
+    let fruits = ["apples", "oranges", "bananas", "pears"];
+
+    for_each_in(fruits, function() {
+      for (let i = 0; i < fruits.length; i++) {
+        console.log(`I like ${fruits[i]}`);
+      } 
+    });
